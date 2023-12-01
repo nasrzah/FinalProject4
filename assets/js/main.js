7c5117f5bb5f8fa59923a79367cab7896c6ac3c0
Hamzah Nasrulloh <nasrullohhamzah@gmail.com>
2023-12-01 11:08:57 +0700

    Menambahkan navbar

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+const iconToggle = document.querySelector('.toggle_icon');
+const navbarMenu = document.querySelector('.menu');
+const menuLinks = document.querySelectorAll('.menu_link');
+const iconClose = document.querySelector('.close_icon');
+
+iconToggle.addEventListener('click', () =>{
+    navbarMenu.classList.toggle('active');
+});
+
+iconClose.addEventListener('click', () =>{
+    navbarMenu.classList.remove('active');
+});
+
+menuLinks.forEach ((menuLink) => {
+    menuLink.addEventListener('click', () =>{
+        navbarMenu.classList.remove('active');
+    })
+});
+
